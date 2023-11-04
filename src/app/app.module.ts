8c3faac6717984fa0decab6d36616a5e12c3c5e4
osakoh <macmive@gmail.com>
2023-11-04 21:58:14 +0000

    starter slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './common/header/header.component';
-import { FooterComponent } from './common/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { MovieListComponent } from './pages/movie-list/movie-list.component'
+import { HeaderComponent } from './common/header/header.component'
+import { FooterComponent } from './common/footer/footer.component'
+import { HomeComponent } from './pages/home/home.component'
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { SliderComponent } from './carousel/slider/slider.component'
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MovieListComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MovieListComponent,
+    SliderComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
