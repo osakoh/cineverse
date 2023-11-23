187101d8e06ca91509db317d9763f2d4eebcde52
osakoh <macmive@gmail.com>
2023-11-23 22:31:13 +0000

    added comments for easier understanding

--- src/app/constants/image-configuration.ts
+++ src/app/constants/image-configuration.ts
@@ -6,7 +6,8 @@ export const SECURE_BASE_URL = 'https://image.tmdb.org/t/p/'
  *
  * Object.freeze: makes the array immutable, preventing any modifications to its size or contents
  *
- * 'as const': asserts that all members of the array are of specific literal types(each member of the array is treated as a constant with a specific and unchangeable value)
+ * 'as const': asserts that that each array element is treated as a unique, unchangeable constant, not just a general string
+ *           that is,'w300' is recognized specifically as the string 'w300'. This approach turns array elements into distinct constants, enhancing type checking and ensuring their values remain constant.
  */
 export const BACKDROP_SIZES = Object.freeze([
   'w300',
