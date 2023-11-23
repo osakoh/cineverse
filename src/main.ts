ec1c149fc523f4b197ee4c9235c0d9939ce7b090
osakoh <macmive@gmail.com>
2023-11-23 22:27:20 +0000

    added comments

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app/app.module'
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+/**
+ * platformBrowserDynamic().bootstrapModule(AppModule): creates a platform instance and then bootstraps the AppModule on this platform.
+ * Bootstrapping(in Angular) is the process of initializing and launching the application.
+ * The bootstrapModule method uses dynamic compilation, allowing Angular to compile the application in the browser at runtime
+ */
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err))
